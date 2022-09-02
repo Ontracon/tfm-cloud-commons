5ab234253645bf3088b88140813b7fd9883ad127
Joerg Kritzen <j.kritzen@ontracon.de>
2022-09-02 15:11:35 +0200

    test

---
+++ examples/11-simple/variables.tf
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Global Variables
+# ---------------------------------------------------------------------------------------------------------------------
+variable "cloud_region" {
+  type        = string
+  description = "Define the cloud region to use (AWS Region / Azure Location) which tf should use."
+}
+
+variable "global_config" {
+  type = object({
+    env             = string
+    customer_prefix = string
+    project         = string
+    application     = string
+    costcenter      = string
+  })
+  description = "Global config Object which contains the mandatory informations within OTC."
+}
