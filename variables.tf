b95b2fffb256e968c5e89e8b217056bd9641195c
Joerg Kritzen <43033899+jkritzen@users.noreply.github.com>
2022-10-14 15:51:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -33,7 +33,6 @@ variable "global_config" {
     project         = string
     application     = string
     costcenter      = string
-    customer = string
   })
   description = "Global config Object which contains the mandatory informations within OTC."
 
